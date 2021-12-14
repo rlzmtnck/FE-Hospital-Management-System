120df6bb14f96e2108a00ef6276bebc35c5c62af
Aditya Himawab <adityahimaone@gmail.com>
2021-12-14 23:58:56 +0700

    add dropdown

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import PropTypes from "prop-types";
 import AppBar from "@mui/material/AppBar";
 import Drawer from "@mui/material/Drawer";
@@ -11,6 +11,7 @@ import ListItemText from "@mui/material/ListItemText";
 import { HomeIcon, MenuIcon } from "@heroicons/react/solid";
 import { PencilAltIcon, PlusCircleIcon } from "@heroicons/react/outline";
 import { useNavigate, useLocation } from "react-router-dom";
+import { Menu, Transition } from "@headlessui/react";
 import format from "date-fns/format";
 
 const drawerWidth = 240;
@@ -84,11 +85,57 @@ export default function Layout(props) {
             <MenuIcon className="h-5 w-5" />
           </IconButton>
           <div className="flex w-full">
-            <div className="grow">
+            <div className="grow py-2">
               <h2 className="text-black">{format(new Date(), "d MMMM Y")}</h2>
             </div>
             <div className="flex-none">
-              <h1 className="text-black font-semibold">Hi, Admin</h1>
+              <Menu as="div" className="relative inline-block text-left">
+                <Menu.Button className="inline-flex justify-center w-full px-4 py-2 font-medium text-black text-lg rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                  Hi, Admin
+                </Menu.Button>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="px-1 py-1 ">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            className={`${
+                              active
+                                ? "bg-maingreen-200 text-white"
+                                : "text-gray-900"
+                            } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                            href="/account-settings"
+                          >
+                            Account settings
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            className={`${
+                              active
+                                ? "bg-maingreen-200 text-white"
+                                : "text-gray-900"
+                            } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                            href="/logout"
+                          >
+                            Logout
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </div>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
             </div>
           </div>
         </Toolbar>
@@ -148,7 +195,7 @@ export default function Layout(props) {
           p: 3,
           width: { sm: `calc(100% - ${drawerWidth}px)` },
         }}
-        className="bg-gray-100"
+        className="bg-gray-100  max-h-full"
       >
         <div className="mt-12">{props.children}</div>
       </Box>
